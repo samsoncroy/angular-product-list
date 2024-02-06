d68094d2ef55c0bc58f65fade16263174226ca33
samsoncroy <samsoncroy@gmail.com>
2024-02-06 11:17:49 +0530

    Unit-test error correction

--- src/app/products-cards/products-cards.component.spec.ts
+++ src/app/products-cards/products-cards.component.spec.ts
@@ -8,12 +8,32 @@ describe('ProductsCardsComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ProductsCardsComponent],
-      imports: [CommonModule],
-    });
+      imports: [ProductsCardsComponent],
+    }).compileComponents();
+  });
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ProductsCardsComponent);
     component = fixture.componentInstance;
+    component.products = [
+      {
+        id: 1,
+        name: 'Product Name',
+        brand: 'Brand Name',
+        imageUrl: 'http://example.com/product.jpg',
+        price:  100.00,
+        description: 'First product description',
+      },
+      {
+        id: 2,
+        name: 'Another Product',
+        brand: 'Other Brand',
+        imageUrl: 'http://example.com/another-product.jpg',
+        price:  200.00,
+        description: 'Second product description',
+      },
+    ];
+    fixture.detectChanges();
   });
 
   it('should create the component', () => {
@@ -25,23 +45,26 @@ describe('ProductsCardsComponent', () => {
   });
 
   it('should set selectedDescription when showDescription is called', () => {
-    const index = 0;
-    const expectedDescription = component.products[index].description;
-
-    component.showDescription(index);
-
-    expect(component.selectedDescription).toEqual(expectedDescription);
+    const index =  0;
+    if (component.products && component.products[index]) {
+      const expectedDescription = component.products[index].description;
+      component.showDescription(index);
+      expect(component.selectedDescription).toEqual(expectedDescription);
+    } else {
+      fail('Product at index does not exist');
+    }
   });
 
   it('should update selectedDescription when showDescription is called with a different index', () => {
-    const initialIndex = 0;
-    const updatedIndex = 1;
-
-    component.showDescription(initialIndex);
-    const initialDescription = component.selectedDescription;
-
-    component.showDescription(updatedIndex);
-
-    expect(component.selectedDescription).not.toEqual(initialDescription);
+    const initialIndex =  0;
+    const updatedIndex =  1;
+    if (component.products && component.products[initialIndex] && component.products[updatedIndex]) {
+      component.showDescription(initialIndex);
+      const initialDescription = component.selectedDescription;
+      component.showDescription(updatedIndex);
+      expect(component.selectedDescription).not.toEqual(initialDescription);
+    } else {
+      fail('One or both products at indices do not exist');
+    }
   });
 });
